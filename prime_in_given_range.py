ec34fe79bd7f0fa044e09873c7fe7f2610cd5a6c
abhinits2046 <abhishekyadav.yadav53@gmail.com>
2019-06-18 13:59:49 +0530

    Python scripts

--- prime_in_given_range.py
+++ prime_in_given_range.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu June  06 18:40:24 2019
+Created on Thu June  18 18:40:24 2019
 
 @author: Abhinits2046
 """
 # we assume r is range
-r = int(input("Enter the range"))
-n = 2;
-while n>2:
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
