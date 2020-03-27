b83cd5faacef1c20d1d97834991a28eb3d6653e6
abbisk <abbiskpro@gmail.com>
2020-03-27 08:23:35 +0530

    count a letter in a string

---
+++ String/print_string.py
@@ -0,0 +1,23 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index+1
+
+
+# Print all letters of the String
+print('\n')
+fruit = 'banana'
+loop=0
+while loop < len(fruit):
+    letter1 = fruit[loop]
+    print(letter1)
+    loop = loop+1
+
+
+# Print all the letters in String
+print('\n')
+fruit2 = 'apple'
+for letter3 in fruit2 :
+    print(letter3)
