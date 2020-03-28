30527c47dd13b113a3ead5a3e86e7fe6e875cb68
abbisk <abbiskpro@gmail.com>
2020-03-28 17:14:21 +0530

    string operations

---
+++ String/String_operations.py
@@ -0,0 +1,11 @@
+fruit = 'apple'
+user = input("Enter the fruits name \n")
+
+if user == 'apple':
+    print("All right it's apple")
+
+if user < 'apple':
+    print("String is less in length than apple")
+
+if user > 'apple':
+    print("String is greater in length than apple") 
