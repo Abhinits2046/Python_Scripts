a05a9eec9ce9dfb519d1c5dc115f4b36d42485a3
abbisk <abbiskpro@gmail.com>
2020-04-24 22:26:56 +0530

    list basic example

--- String/file_handling.py
+++ String/file_handling.py
@@ -7,9 +7,10 @@ print('Line count : ', count)
 # Reading the whole file
 fhand = open('/home/abbisk/Documents/Code/python-scripts/String/greetings.txt')
 inp = fhand.read()
+inp1 = inp.rstrip()
 print("Length of the File : " + str(len(inp)))
-# print(inp)
-print(inp[0:5])
+print(inp.upper())  # Change the whole file text in capital latter
+# print(inp[0:5])
 
 # Searching through the file
 fhand = open('/home/abbisk/Documents/Code/python-scripts/String/greetings.txt')
@@ -23,3 +24,22 @@ for line in fhand:
     line = line.rstrip()
     if line.startswith('Thanks'):
         print(line)
+
+# Method 2 for Remove blank line
+fhand = open('/home/abbisk/Documents/Code/python-scripts/String/greetings.txt')
+for line in fhand:
+    if not line.startswith('Thanks'):
+        continue
+    print(line)
+
+# Letting the use chose the file name
+print("Letting the use choose the file name")
+fname = input("Enter the file name: ")
+fhand = open(fname)
+count = 0
+for line in fhand:
+    count = count+1
+print("No of line in the file are ", count)
+
+# Writing files
+fout = open()
