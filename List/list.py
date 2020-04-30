13d64ca5e5a511a06abab6c0d365406f6d01732e
abbisk <abbiskpro@gmail.com>
2020-04-30 08:37:05 +0530

    in and range function use

--- List/list.py
+++ List/list.py
@@ -1,1 +1,28 @@
 print(['Abhishek','B.Tech','abbiskpro@gmail.com',8486802046])
+
+for i in [5,4,3,2,1]:
+    print(i,end =' ')
+
+# Looking inside list
+fruits = ['banana','apple','pineapple','grapes','orange']
+print(fruits)
+index = input('Enter the index number')
+print(fruits[int(index)])
+
+# use of in operator
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+print('Edan' in cheeses)
+print('Edam' in cheeses)
+
+# length of the list
+greet = 'Hello welcome you all'
+print("Length of the string : ", len(greet))
+
+# Range function
+print(range(4))
+friends = ['Abhsishek','Vikash','Ankit','Dayaram','Sandeep']
+print(range(len(friends)))
+
+# print the friends
+for i in friends:
+    print(i, end = "\t")
