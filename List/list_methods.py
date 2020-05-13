d0eb6b5668990a2c2082072a074b3c0c25389073
abbisk <abbiskpro@gmail.com>
2020-05-13 20:10:52 +0530

    add Sorting and deleting methods

---
+++ List/list_methods.py
@@ -0,0 +1,23 @@
+# Append
+t = ['a','b','c','d','e','f','g','h','i']
+t.append(input('Input for append method : '))
+print(t)
+
+# Extend 
+# Takes list as an arguement and append all the elements
+t1 = ['k','l']
+inp = list(map(str, input("Enter the elements Extend : ")))
+t1.extend(inp)
+print(t1)
+
+# Sorting
+print("Sorting")
+t2 = ['d', 'c', 'e', 'b', 'a']
+t2.sort()
+print(t2)
+
+# Deleting elements
+print("Deleting elements")
+t.pop(1)
+t.remove('a')
+print(t)
