f010510cdb391b283aa218839213123ca8f7f031
abbisk <abbiskpro@gmail.com>
2020-08-28 15:19:05 +0530

    Functions added to dArray

--- Array/dynamicArray.py
+++ Array/dynamicArray.py
@@ -8,12 +8,44 @@ class DynamicArray(object):
         self.capacity=1 # Default Capacity
         self.A = self.make_array(self.capacity)
 
-        def __len__(self):
-            # Return number of element stored in array
-            return self.n
+    def __len__(self):
+        # Return number of element stored in array
+        return self.n
 
-        def __getitems__(self,k):
-            
-            # Return element index k
-            if not 0<=k <self.n:
-                return IndexError("K is out of bonds!")
+    def __getitems__(self,k):
+        
+        # Return element index k
+        if not 0<=k <self.n:
+            return IndexError("K is out of bonds!")
+            # Check it k index is in bound
+        
+        return self.A[k] # Retrive calue from array at kth index
+
+    def append(self,ele):
+        # Add element at the end of array
+
+        if self.n == self.capacity:
+            self._resize(2*self.capacity)
+
+        self.A[self.n] = ele
+        self.n += 1
+
+    def _resize(self,new_cap):
+        # Return internal array to capacity new_cap
+        
+        B = self.make_array(new_cap) #New bigger array
+
+        for k in range(self.n):
+            B[k]=self.A[k]
+
+        self.A=B
+        self.capacity = new_cap
+
+    def make_array(self,new_cap):
+
+        return(new_cap * ctypes.py_object)()
+
+arr = DynamicArray()
+
+arr.append(1)
+print(len(arr))
