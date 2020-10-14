5f7ecebd27dc07a03c4923cc513b9600b0152217
abbisk <abbiskpro@gmail.com>
2020-10-14 22:56:16 +0530

    Prime number checker added

---
+++ List/primenum.py
@@ -0,0 +1,16 @@
+# prime number upto n
+def prime(n):
+    flist = []
+    for i in range(1,n+1):
+        if i ==2:
+            flist.append(i)
+        if i>1:
+            for j in range(2,i+1):
+                if (i%j==0):
+                    break
+                if j==i-1:
+                    flist.append(i)
+                    break
+    return flist
+r = int(input("Enter prime range: "))
+print(prime(r))
