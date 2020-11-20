4e31c812b4839611f92ac2d16501427ec0022c31
abbisk <abbiskpro@gmail.com>
2020-11-20 15:08:11 +0530

    Script moved to related folder

--- List/SelectionSort.py
+++ List/SelectionSort.py
@@ -1,4 +1,5 @@
-l= list(map(int,input("Enter the elements for selection sort: ").split()))
+# l= list(map(int,input("Enter the elements for selection sort: ").split()))
+l= list(range(10000,0,-1))
 
 def SelectionSort(l):
     for start in range(len(l)):
